76eb3b98a8f39f13379ff6a21219a457cb3f24b4
Passion McNabb <prmcnabb@gmail.com>
2016-04-08 14:41:57 -0500

    added click events

--- js/script.js
+++ js/script.js
@@ -1,9 +1,48 @@
 
-document.getElementById("topleft");
-  console.log(topleft);
+var box1 = document.getElementById("topleft");
+box1.addEventListener("click", function(){
+  alert(box1.innerHTML);
+});
+
+var box2 = document.getElementById("topcenter");
+box2.addEventListener("click", function(){
+  alert(box2.innerHTML);
+});
 
 
+var box3 = document.getElementById("topright");
+box3.addEventListener("click", function(){
+  alert(box3.innerHTML);
+});
 
+var box4 = document.getElementById("middleleft");
+box4.addEventListener("click", function(){
+  alert(box4.innerHTML);
+});
 
+var box5 = document.getElementById("middlecenter");
+box5.addEventListener("click", function(){
+  alert(box5.innerHTML);
+});
+
+var box6 = document.getElementById("middleright");
+box6.addEventListener("click", function(){
+  alert(box6.innerHTML);
+});
+
+var box7 = document.getElementById("bottomleft");
+box7.addEventListener("click", function(){
+  alert(box7.innerHTML);
+});
+
+var box8 = document.getElementById("bottomcenter");
+box8.addEventListener("click", function(){
+  alert(box8.innerHTML);
+});
+
+var box9 = document.getElementById("bottomright");
+box9.addEventListener("click", function(){
+  alert(box9.innerHTML);
+});
 
 
