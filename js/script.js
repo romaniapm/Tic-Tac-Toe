6c3e89a41668fcaebc93d64bb00bcb069ac3df5a
Passion McNabb <prmcnabb@gmail.com>
2016-04-11 16:13:50 -0500

    Fixed bug

--- js/script.js
+++ js/script.js
@@ -10,75 +10,66 @@ function activePlayer() {
   activeplayer = player1;
 }}
 
+function setXO(box) {
+  if (box.innerHTML.length === 0){
+    box.innerHTML = activeplayer.token;
+    activePlayer();
+    checkWinner();
+  }
+}
+
 var box1 = document.getElementById("topleft");
 box1.addEventListener("click", function(){
-  box1.innerHTML = activeplayer.token;
-  activePlayer();
-  checkWinner();
+  setXO(box1);
 });
 
 
 var box2 = document.getElementById("topcenter");
 box2.addEventListener("click", function(){
-  box2.innerHTML = activeplayer.token;
-  activePlayer();
-  checkWinner();
+  setXO(box2);
 });
 
 
 var box3 = document.getElementById("topright");
 box3.addEventListener("click", function(){
-  box3.innerHTML = activeplayer.token;
-  activePlayer();
-  checkWinner();
+  setXO(box3);
 });
 
 
 var box4 = document.getElementById("middleleft");
 box4.addEventListener("click", function(){
-  box4.innerHTML = activeplayer.token;
-  activePlayer();
-  checkWinner();
+  setXO(box4);
+
 });
 
 
 var box5 = document.getElementById("middlecenter");
 box5.addEventListener("click", function(){
-  box5.innerHTML = activeplayer.token;
-  activePlayer();
-  checkWinner();
+  setXO(box5);
 });
 
 
 var box6 = document.getElementById("middleright");
 box6.addEventListener("click", function(){
-  box6.innerHTML = activeplayer.token;
-  activePlayer();
-  checkWinner();
+  setXO(box6);
 });
 
 
 var box7 = document.getElementById("bottomleft");
 box7.addEventListener("click", function(){
-  box7.innerHTML = activeplayer.token;
-  activePlayer();
-  checkWinner();
+  setXO(box7);
 });
 
 
 var box8 = document.getElementById("bottomcenter");
 box8.addEventListener("click", function(){
-  box8.innerHTML = activeplayer.token;
-  activePlayer();
-  checkWinner();
+  setXO(box8);
 });
 
 
 var box9 = document.getElementById("bottomright");
 box9.addEventListener("click", function(){
-  box9.innerHTML = activeplayer.token;
-  activePlayer();
-  checkWinner();
+  setXO(box9);
 });
 
 function checkWinner (){
