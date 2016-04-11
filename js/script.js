314fec8ed101777349340845ffa83e205b1b9c4d
Passion McNabb <prmcnabb@gmail.com>
2016-04-11 10:18:20 -0500

    x

--- js/script.js
+++ js/script.js
@@ -1,48 +1,86 @@
 
+var player1 = {token:"X"}
+var player2 = {token:"O"}
+var activeplayer = player1
+
+function activePlayer() {
+ if(activeplayer == player1){
+  activeplayer = player2;
+} else {
+  activeplayer = player1;
+}}
+
 var box1 = document.getElementById("topleft");
 box1.addEventListener("click", function(){
-  alert(box1.innerHTML);
+  box1.innerHTML = activeplayer.token;
+
+// if activeplayer =player1, then = player2
+
+activePlayer();
+
 });
 
 var box2 = document.getElementById("topcenter");
 box2.addEventListener("click", function(){
-  alert(box2.innerHTML);
+   box2.innerHTML = activeplayer.token;
+ activePlayer();
 });
+
 
 
 var box3 = document.getElementById("topright");
 box3.addEventListener("click", function(){
-  alert(box3.innerHTML);
+  box3.innerHTML = activeplayer.token;
+ activePlayer();
 });
+
+
 
 var box4 = document.getElementById("middleleft");
 box4.addEventListener("click", function(){
-  alert(box4.innerHTML);
+  box4.innerHTML = activeplayer.token;
+ activePlayer();
 });
+
+
 
 var box5 = document.getElementById("middlecenter");
 box5.addEventListener("click", function(){
-  alert(box5.innerHTML);
+  box5.innerHTML = activeplayer.token;
+ activePlayer();
 });
+
+
 
 var box6 = document.getElementById("middleright");
 box6.addEventListener("click", function(){
-  alert(box6.innerHTML);
+  box6.innerHTML = activeplayer.token;
+ activePlayer();
 });
+
+
 
 var box7 = document.getElementById("bottomleft");
 box7.addEventListener("click", function(){
-  alert(box7.innerHTML);
+  box7.innerHTML = activeplayer.token;
+ activePlayer();
 });
+
+
 
 var box8 = document.getElementById("bottomcenter");
 box8.addEventListener("click", function(){
-  alert(box8.innerHTML);
+  box8.innerHTML = activeplayer.token;
+ activePlayer();
 });
+
+
 
 var box9 = document.getElementById("bottomright");
 box9.addEventListener("click", function(){
-  alert(box9.innerHTML);
+  box9.innerHTML = activeplayer.token;
+ activePlayer();
 });
+
 
 
